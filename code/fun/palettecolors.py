ae97946bb278a0a595415c8bdb6270de0ccf8930
Thomas Neff <thms.neff@gmail.com>
2024-10-31 15:03:15 +0100

    update color palette

--- code/fun/palettecolors.py
+++ code/fun/palettecolors.py
@@ -8,20 +8,20 @@ Author: Thomas Neff
 Date: 2024-10-28 16:29:12
 """
 
-pal_sugroup={'WNT':'royalblue',
+pal_subgroup={'WNT':'royalblue',
              'SHH':'crimson',
-             'Group3':'yellow',
+             'Group3':'gold',
              'Group4':'forestgreen'}
-             
-pal_subtype={'WNTalpha':'royalblue',
-             'WNbeta':'lightskyblue',
-             'SSHalpha':'crimson',
-             'SSHbeta':'peru',
-             'SSHgamma':'orchid',
-             'SSHsigma':'lightpink',
-             'Group3alpha':'yellow',
-             'Group3beta':'khaki',
-             'Group3gamma':'gold',
-             'Group4gamma':'forestgreen',
-             'Group4beta':'palegreen',
-             'Group4alpha':'springgreen'}
+
+pal_subtype={'WNT_alpha':'royalblue',
+             'WNT_beta':'lightskyblue',
+             'SHH_alpha':'crimson',
+             'SHH_beta':'peru',
+             'SHH_gamma':'orchid',
+             'SHH_delta':'lightpink',
+             'Group3_alpha':'gold',
+             'Group3_beta':'khaki',
+             'Group3_gamma':'orange',
+             'Group4_gamma':'forestgreen',
+             'Group4_beta':'palegreen',
+             'Group4_alpha':'springgreen'}
