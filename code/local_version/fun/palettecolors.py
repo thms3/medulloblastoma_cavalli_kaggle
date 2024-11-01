fdd9341367e145e55ad2731d3ba48e75eaea421c
Thomas Neff <thms.neff@gmail.com>
2024-11-01 11:29:15 +0100

    add local and kaggle version

---
+++ code/local_version/fun/palettecolors.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Project: Medulloblastoma Cavalli Kaggle Project
+Description: Color palettes
+Author: Thomas Neff
+Date: 2024-10-28 16:29:12
+"""
+
+pal_subgroup={'WNT':'royalblue',
+             'SHH':'crimson',
+             'Group3':'gold',
+             'Group4':'forestgreen'}
+
+pal_subtype={'WNT_alpha':'royalblue',
+             'WNT_beta':'lightskyblue',
+             'SHH_alpha':'crimson',
+             'SHH_beta':'peru',
+             'SHH_gamma':'orchid',
+             'SHH_delta':'lightpink',
+             'Group3_alpha':'gold',
+             'Group3_beta':'khaki',
+             'Group3_gamma':'orange',
+             'Group4_gamma':'forestgreen',
+             'Group4_beta':'palegreen',
+             'Group4_alpha':'springgreen'}
